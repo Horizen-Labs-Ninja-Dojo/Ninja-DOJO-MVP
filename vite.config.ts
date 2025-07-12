fc2c3e47605454971d752c83e836cfacc2a6e0e7
ninjadev710 <alexander0110827@outlook.com>
2025-07-12 13:34:32 -0500

    fix:Update UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tagger from 'lovable-ci'
+import tagger from 'lovable-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
